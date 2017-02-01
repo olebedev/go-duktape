812e77b82de0bce53bcee4a1e4a7d9ee02e6591c
Nick Randall <nick.randall@domo.com>
2017-01-31 21:49:19 -0700

    trying to pass test so this PR can be merged

--- api_test.go
+++ api_test.go
@@ -18,12 +18,11 @@ func (s *DuktapeSuite) TestPevalFile_Error(c *C) {
 	c.Assert(err.(*Error).Message, Equals, "no sourcecode")
 }
 
-// This test panics.
-// func (s *DuktapeSuite) TestPcompileString(c *C) {
-// 	err := s.ctx.PcompileString(CompileFunction, "foo")
-// 	c.Assert(err.(*Error).Type, Equals, "SyntaxError")
-// 	c.Assert(err.(*Error).LineNumber, Equals, 1)
-// }
+func (s *DuktapeSuite) TestPcompileString(c *C) {
+	err := s.ctx.PcompileString(CompileEval, "foo(")
+	c.Assert(err.(*Error).Type, Equals, "SyntaxError")
+	c.Assert(err.(*Error).LineNumber, Equals, 1)
+}
 
 func (s *DuktapeSuite) TestPushErrorObject(c *C) {
 	s.ctx.PushErrorObject(ErrType, "Got an error thingy: %v", 5)
