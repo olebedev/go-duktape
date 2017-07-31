42c5e738578680abb6377128fa6319fb717ed2bb
Mosolov_S <mosolovs1987@gmail.com>
2017-07-31 22:58:09 +0400

    Added godoc compatiable example

--- example_bytecode_test.go
+++ example_bytecode_test.go
@@ -1,4 +1,17 @@
-package main
+// Example of serialization duktape function to a byte slice; deserialization from byte slice to duktape
+// function and eval in another duktape context.
+//
+// Usecase: get byte slice from duktape function (to possibly later save it to the file, send by network and etc.)
+// Please, keep in mind restrictions for a deserialized duktape function from bytecode:
+// https://github.com/svaarala/duktape/blob/master/doc/bytecode.rst
+//
+//Excerpts:
+//	When to use bytecode dump/load
+//		There are two main motivations for using bytecode dump/load:
+//			-	Performance
+//			-	Obfuscation
+
+package duktape_test
 
 import (
 	"gopkg.in/olebedev/go-duktape.v2"
@@ -6,19 +19,10 @@ import (
 	"errors"
 	"reflect"
 	"unsafe"
+	"fmt"
 )
 
-// Usecase: get byte slice from duktape function (to possibly later save it to the file, send by network and etc.)
-// Please, keep in mind restrictions for a deserialized duktape function from bytecode:
-// https://github.com/svaarala/duktape/blob/master/doc/bytecode.rst
-
-//Excerpts:
-//	When to use bytecode dump/load
-//		There are two main motivations for using bytecode dump/load:
-//			-	Performance
-//			-	Obfuscation
-func main() {
-
+func Example() {
 	// Parenthesis is necessary.
 	// DeserializeAndRunInNewContext assuming that script in jsfunc doesn't have arguments and return string.
 	jsfunc := "(function dump_from() { return 'It\\'s alive!'; })"
@@ -28,26 +32,24 @@ func main() {
 		log.Fatalf("Can't serialize '%s' to bytecode, err: %q.", jsfunc, err)
 	}
 
-	err = deserializeAndRunInNewContext(bytecode)
+	retval, err := deserializeAndRunInNewContext(bytecode)
 	if err != nil {
 		log.Fatalf("Can't deserialize and run '%s' from bytecode, err: %q.", jsfunc, err)
 	}
+
+	fmt.Print(retval)
+
+	// Output:
+	// It's alive!
 }
 
 func getSerializedFunc(script string) ([]byte, error) {
-	log.Printf("Begin serialization of the duktape function:\n%s.", script)
-
 	ctx := duktape.New()
 
 	ctx.EvalLstring(script, len(script))
-		// Get the type of an element on top of the context stack
-	typ := ctx.GetType(-1)
-	log.Printf("Type after EvalLstring is: %v.", typ)
 
-		// Transmute function into serializable duktape bytecode
+	// Transmute function into serializable duktape bytecode
 	ctx.DumpFunction()
-	typ = ctx.GetType(-1)
-	log.Printf("Type after DumpFunction is: %v.", typ)
 
 	// void *duk_get_buffer(duk_context *ctx, duk_idx_t idx, duk_size_t *out_size);
 	// typedef size_t duk_size_t;
@@ -60,11 +62,10 @@ func getSerializedFunc(script string) ([]byte, error) {
 	if uintptr(rawmem) == uintptr(0) {
 		return nil, errors.New("Can't interpret bytecode dump as a valid, non-empty buffer.")
 	}
-
 	rawmemslice := *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{Data:uintptr(rawmem), Len:sz, Cap:sz}))
 
 	// Creating another slice for return is necessary because rawmemslice pointing at memory that belongs to
-	// current context that will be freed during execution of DestroyHeap().
+	// current context. That memory will be freed during execution of DestroyHeap().
 	retval := make([]byte, sz)
 	copy(retval, rawmemslice)
 
@@ -72,39 +73,35 @@ func getSerializedFunc(script string) ([]byte, error) {
 	// yourself when you're done using a context.
 	ctx.DestroyHeap()
 
-	log.Printf("Finished serialization of the duktape function:\n%s, sizeof:%d.", script, sz)
-
 	return retval, nil
 }
 
-func deserializeAndRunInNewContext(bc []byte) error {
+func deserializeAndRunInNewContext(bc []byte) (string, error) {
 	ctx := duktape.New()
 
+	//creating buffer on the context stack
 	rawmem := ctx.PushBuffer(len(bc), false)
 	if uintptr(rawmem) == uintptr(0) {
-		return errors.New("Can't push buffer to the context stack.")
+		return "", errors.New("Can't push buffer to the context stack.")
 	}
+
+	//copying bytecode into the created buffer
 	rawmemslice := *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{Data:uintptr(rawmem), Len:len(bc), Cap:len(bc)}))
 	copy(rawmemslice, bc)
 
-	typ := ctx.GetType(-1)
-	log.Printf("Type after PushBuffer and copying bytecode into it is: %v.", typ)
-
+	// Transmute duktape bytecode into duktape function
 	ctx.LoadFunction()
-	typ = ctx.GetType(-1)
-	log.Printf("Type after LoadFunction (transforming bytecode to duktape function): %v.", typ)
 
-	// Call the function on top of a stack, function must be without arguments.
+	// Call the function on top of a stack, example function doesn't have arguments.
 	ctx.Call(0)
-	typ = ctx.GetType(-1)
-	log.Printf("Return value type of a deserialized duktape function is: %v", typ)
 
+	//example function return value is string
 	retval := ctx.GetString(-1)
 	log.Printf("Return value is: %s", retval)
 
 	// To prevent memory leaks, don't forget to clean up after
 	// yourself when you're done using a context.
-	ctx.DestroyHeap()	
-	
-	return nil
+	ctx.DestroyHeap()
+
+	return retval, nil
 }
